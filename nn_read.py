49fd0e3c6d160beea6d9e0f377521de567be399a
biagio <paparell@ins.uni-bonn.de>
2020-10-27 11:47:55 +0100

    Improved the visualization for the NN convergence

--- nn_read.py
+++ nn_read.py
@@ -20,7 +20,7 @@ def auxiliary_gradient(params_together):
             nn_potential.y, b2, b3, b4, W2, W3, W4)
 
 
-STUDY_SINGLE_CHAIN = True #False
+STUDY_SINGLE_CHAIN = False #True #False
 STUDY_CONVERGENCE = True#True
 
 if STUDY_SINGLE_CHAIN:
@@ -221,6 +221,7 @@ if STUDY_CONVERGENCE:
     samples_file.close()
     print("Read", len(X), "samples of dimension", len(X[0]))
 
+    m = len(X[0])
     # Computing the confidence interval for each marginal
     for i in range(m):
         # Compute the 95% confidence interval
@@ -230,9 +231,60 @@ if STUDY_CONVERGENCE:
         print("95% Confidence Interval: [",
                         mean-2.*sigma, " ", mean+2.*sigma, "]")
 
-    # Plot the expectations' distribution
-    for i in range(m):
-        plt.subplot(m, 1, i+1)
-        plt.hist(X[:,i], 50, density=True)
-    plt.suptitle("Convergence analysis. Gaussian = WIN")
+    # Plot the non-clustered expectations energy distribution
+    for i in range(23):
+        plt.subplot(5, 5, i+1)
+        # If 0 or 1, we are with parameter b2
+        if (i < 2):
+            plt.hist(X[:,i], 50, density=True, color='green')
+            plt.title('b2[' + str(i+1) + ']')
+        elif (i < 5):
+            plt.hist(X[:,i], 50, density=True, color='darkorange')
+            plt.title('b3[' + str(i-1) + ']')
+        elif (i < 7):
+            plt.hist(X[:,i], 50, density=True, color='magenta')
+            plt.title('b4[' + str(i-4) + ']')
+        elif (i < 11):
+            if (i == 7):
+                coeffi, coeffj = 1, 1
+            elif (i == 8):
+                coeffi, coeffj, = 1, 2
+            elif (i == 9):
+                coeffi, coeffj = 2, 1
+            elif (i == 10):
+                coeffi, coeffj = 2, 2
+            plt.hist(X[:,i], 50, density=True, color='steelblue')
+            plt.title('W2[' + str(coeffi) + ',' + str(coeffj) + ']')
+        elif (i < 17):
+            if (i == 11):
+                coeffi, coeffj = 1, 1
+            elif i == 12:
+                coeffi, coeffj = 1, 2
+            elif i == 13:
+                coeffi, coeffj = 2, 1
+            elif i == 14:
+                coeffi, coeffj = 2, 2
+            elif i == 15:
+                coeffi, coeffj = 3, 1
+            elif i == 16:
+                coeffi, coeffj = 3, 2
+            plt.hist(X[:,i], 50, density=True, color='grey')
+            plt.title('W3[' + str(coeffi) + ',' + str(coeffj)+ ']')
+        elif (i < 23):
+            if (i == 17):
+                coeffi, coeffj = 1, 1
+            elif i == 18:
+                coeffi, coeffj = 1, 2
+            elif i == 19:
+                coeffi, coeffj = 1, 3
+            elif i == 20:
+                coeffi, coeffj = 2, 1
+            elif i == 21:
+                coeffi, coeffj = 2, 2
+            elif i == 22:
+                coeffi, coeffj = 2, 3
+            plt.hist(X[:,i], 50, density=True, color='darkblue')
+            plt.title('W4[' + str(coeffi) + ',' + str(coeffj)+ ']')
+        plt.grid(True)
+    plt.suptitle("Distributions of the EXPECTATIONS. Gaussian = OK")
     plt.show()
